ff6ba27d8ffdf633ffd91753e15d8f96ef0bf214
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-02-16 09:33:08 -0600

    Practiced looping through arrays

--- JavaScript/Arrays/Arrays.js
+++ JavaScript/Arrays/Arrays.js
@@ -6,3 +6,16 @@ console.log(names.length);
 
 // Access @ index
 console.log(names[3], names[4], names[10], names[names.length - 1]);
+
+// Loop
+names.forEach((name, index) => {
+    console.log(`Hello ${name}, at index ${index}`);
+});
+
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+
+for (let i = names.length - 1; i >= 0; i--) {
+    console.log(`Hi ${names[i]}, at ${i}`);
+}

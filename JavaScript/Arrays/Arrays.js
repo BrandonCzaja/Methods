0c0acbae30186fef91fb9840960dad11d14c3952
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-02-16 09:29:50 -0600

    Practiced accessing array data

---
+++ JavaScript/Arrays/Arrays.js
@@ -0,0 +1,8 @@
+// Create an array
+const names = ["Brandon", "Nora", "Zach", "Erin", "Vlad", "Violetta", "Colin"];
+
+// Length
+console.log(names.length);
+
+// Access @ index
+console.log(names[3], names[4], names[10], names[names.length - 1]);
